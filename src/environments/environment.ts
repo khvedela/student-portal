d2e49b7a1eb5538b284c02f60b8b08e5694c7f19
David Khvedelidze <d.khv08@gmail.com>
2024-12-15 23:13:59 +0100

    fixed api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://tropical-edythe-davidslab-7b6f99be.koyeb.app/api'
+  apiUrl: 'https://tropical-edythe-davidslab-7b6f99be.koyeb.app'
 };
